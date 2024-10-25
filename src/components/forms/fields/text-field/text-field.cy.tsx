1badc522a517d72de4f6375f5f22d24683136ec7
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-10-25 22:34:00 +0530

    Improve test coverage in text field

--- src/components/forms/fields/text-field/text-field.cy.tsx
+++ src/components/forms/fields/text-field/text-field.cy.tsx
@@ -2,6 +2,8 @@ import { TextField } from "./text-field.component";
 import { FormWrapper } from "../../../../test-utils/forms-test-utils";
 import { useForm } from "react-hook-form";
 import { AccountCircle } from "@mui/icons-material";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { object, string } from "yup";
 
 const TestComponent = ({
   dataTestId,
@@ -11,6 +13,31 @@ const TestComponent = ({
   Icon?: any;
 }) => {
   const formMethods = useForm();
+
+  return (
+    <FormWrapper formMethods={formMethods}>
+      <TextField name="testname" data-testid={dataTestId} Icon={Icon} />
+    </FormWrapper>
+  );
+};
+
+const TestComponentWithError = ({
+  dataTestId,
+  Icon,
+}: {
+  dataTestId?: string;
+  Icon?: any;
+}) => {
+  const formMethods = useForm({
+    resolver: yupResolver(
+      object().shape({
+        testname: string().required(),
+      })
+    ),
+    defaultValues: {
+      testname: "",
+    },
+  });
 
   return (
     <FormWrapper formMethods={formMethods}>
@@ -34,5 +61,14 @@ describe("<TextField />", () => {
   it("should load with Icon", () => {
     cy.mount(<TestComponent Icon={AccountCircle} />);
     cy.findByTestId("TextField.Icon").should("exist");
+  });
+
+  it("should handle error", () => {
+    cy.mount(<TestComponentWithError />);
+    cy.findByTestId("TestForm.Button").click();
+    cy.findByTestId("TextField.HelperText").should(
+      "have.text",
+      "testname is a required field"
+    );
   });
 });
