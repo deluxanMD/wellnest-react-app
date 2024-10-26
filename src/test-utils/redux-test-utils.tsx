1f284cea96dd9f2889b0d9426d0cde8aefbbd223
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-10-26 08:15:13 +0530

    Reduce target code coverage to 90%

--- src/test-utils/redux-test-utils.tsx
+++ src/test-utils/redux-test-utils.tsx
@@ -1,8 +1,14 @@
 import { CssBaseline } from "@mui/material";
+import { render, RenderOptions } from "@testing-library/react";
 import ThemeWrapper from "helpers/theme/theme.wrapper";
-import { ReactNode } from "react";
+import { PropsWithChildren, ReactNode } from "react";
 import { Provider } from "react-redux";
-import { store } from "store";
+import { AppStore, RootState, setupStore, store } from "store";
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
+  preloadedState?: Partial<RootState>;
+  store?: AppStore;
+}
 
 type ReduxWrapperProps = {
   children: ReactNode;
@@ -17,4 +23,26 @@ export const ReduxWrapper = ({ children }: ReduxWrapperProps) => {
       </ThemeWrapper>
     </Provider>
   );
+};
+
+export const renderForJest = (
+  ui: React.ReactElement,
+  extendedRenderOptions: ExtendedRenderOptions = {}
+) => {
+  const {
+    preloadedState = {},
+    // Automatically create a store instance if no store was passed in
+    store = setupStore(preloadedState),
+    ...renderOptions
+  } = extendedRenderOptions;
+
+  const Wrapper = ({ children }: PropsWithChildren) => (
+    <Provider store={store}>{children}</Provider>
+  );
+
+  // Return an object with the store and all of RTL's query functions
+  return {
+    store,
+    ...render(ui, { wrapper: Wrapper, ...renderOptions }),
+  };
 };
